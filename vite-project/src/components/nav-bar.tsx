a5e14001135c10469c1bc2e94101a181ce5d8c0f
mahesh narayanan <maheshnarayan97@gmail.com>
2025-04-01 19:50:50 +0530

    initial commit

---
+++ vite-project/src/components/nav-bar.tsx
@@ -0,0 +1,37 @@
+
+import logo from "../assets/logo.png"
+import { FaLinkedin } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+const Navbar = () => {
+    return (
+        <nav className='mb-20 flex items-center justify-between py-6'>
+            <div className='flex flex-shrink-0 items-center'>
+                <img src={logo} alt='logo' className="mx-2 w-25" />
+            </div>
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+                <a
+                    href="https://www.linkedin.com/in/mahesh-narayanan-2339b2175/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ fontSize: "24px" }}
+                >
+                    <FaLinkedin />
+                </a>
+                <FaGithub />
+                <a
+                    href="https://www.instagram.com/_mahesh_narayanan/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ fontSize: "24px" }}
+                >
+                    <FaInstagram />
+                </a>
+                <FaTwitterSquare />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ vite-project/src/constants/index.tsx
@@ -0,0 +1,74 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate Front-End React.js Developer with a strong foundation in building dynamic, responsive, and user-friendly web applications. With 3 years of experience, I specialize in React.js, Next.js, and modern front-end technologies to craft high-performance UI components and seamless user experiences. My expertise includes state management (Redux, Context API), performance optimization, and integrating APIs efficiently. I thrive on turning complex problems into elegant solutions, ensuring pixel-perfect designs, and contributing to scalable and maintainable codebases.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile React TypeScript Developer with 3 years of experience in building scalable, high-performance web applications. My expertise lies in React, Next.js, TypeScript, and modern front-end technologies, enabling me to craft intuitive and efficient user interfaces. Passionate about writing clean, maintainable code, I focus on performance optimization, state management (Redux, Context API), and seamless API integrations.
+My journey in web development started with a deep curiosity about how the web works, which has grown into a continuous pursuit of learning and innovation. I thrive in collaborative environments, enjoy tackling complex challenges, and strive to deliver user-centric solutions.
+Beyond coding, I love exploring new technologies, contributing to open-source projects, and staying up to date with industry trends to enhance my skill set.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - Present",
+    role: "Front end Developer",
+    company: " BV Data solutions",
+    description: `Developed a responsive web application using React.js, Material-UI, and Tailwind CSS, enhancing UI/UX with modern design principles.
+Integrated Telerik Reporting for dynamic report generation, improving data visualization and accessibility.
+ Collaborated with cross-functional teams in Agile environments, actively participating in sprint planning and retrospectives to optimize workflows.
+Ensured high code quality, performance optimization, and web accessibility compliance, adhering to industry best practices.
+Worked in Visual Studio Code, contributing to scalable, maintainable, and efficient front-end solutions.`,
+    technologies: ["Javascript", "React.js", "Typescript", "MUI", "Tailwindcss"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Freelancer",
+    description: `Experienced React.js and TypeScript developer specializing in building responsive, user-friendly, and scalable web applications. Skilled in Next.js, Material-UI, Tailwind CSS, and Node.js, delivering high-performance solutions tailored to client needs. Passionate about clean code, UI/UX optimization, and web accessibility.`,
+    technologies: ["HTML", "CSS", "React JS"],
+  },
+  {
+    year: "2019 - 2020",
+    role: "Software Tester",
+    company: "L2 India",
+    description: `Experienced Software Tester with a strong focus on ensuring software quality, reliability, and performance. Skilled in manual and automated testing, test case creation, bug tracking, and reporting. Proficient in tools like Selenium, JIRA, TestRail, and Postman, ensuring seamless functionality across web and mobile applications. Collaborates with development teams to enhance software quality through rigorous testing strategies and continuous improvement.`,
+    technologies: ["Manual Tester"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kerala, India",
+  phoneNo: "+91 9633903665",
+  email: "blueplanet1909@gmail.com",
+};
